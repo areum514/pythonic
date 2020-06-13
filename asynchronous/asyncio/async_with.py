86557950ae278af4a317c85c871fb575e9b89c7e
areum514 <21500514@handong.edu>
2020-06-13 15:10:45 +0900

    pythonic

---
+++ asynchronous/asyncio/async_with.py
@@ -0,0 +1,34 @@
+import asyncio
+
+
+class AsynchronousReader:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file = None
+
+    async def __aenter__(self):
+        try:
+            self.file = open(self.file_name, "rb")
+        except:
+            print("File open error.")
+            raise Exception
+        else:
+            return self.file
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        self.file.close()
+
+
+async def read_file(file_name):
+    async with AsynchronousReader(file_name) as af:
+        for line in af.readlines():
+            print(line.decode("utf-8").strip())
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+
+    try:
+        loop.run_until_complete(
+            read_file("./python_foudation_misstion_statement"))
+    finally:
+        loop.close()
